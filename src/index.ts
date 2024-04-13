730cf9730e04f7279c23c088c1ac84de7169de8a
vibhor <vibgitcode27@gmail.com>
2024-04-13 23:16:20 +0530

    basic http server

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+import WebSocket, { WebSocketServer } from "ws";
+import http from "http";
+
+const server = http.createServer((request: any, response: any) => {
+  console.log(new Date() + "received request for " + request.url);
+  response.add("hi there");
+});
+
+const wss = new WebSocketServer({ server });
+
+wss.on("connection", function connection(ws) {
+  ws.on("error", console.error);
+
+  ws.on("message", function message(data, isBinary) {
+    wss.clients.forEach(function each(client) {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(data, { binary: isBinary });
+      }
+    });
+  });
+
+  ws.send("Hello! Message From Server!!");
+});
+
+server.listen(8080, () => {
+  console.log(new Date() + " Server is listeningg to post 8080");
+});
